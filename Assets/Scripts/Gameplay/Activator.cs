7dd0676daf632b6a91201723f1574bdf2ffe6205
Felipe S <snowychest@gmail.com>
2020-10-27 01:45:59 -0300

    Making notes fall down and destroying them

---
+++ Assets/Scripts/Gameplay/Activator.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+    
+    public KeyCode key;
+    private SpriteRenderer sr;
+    private bool triggerIsActive = false;
+    private GameObject note;
+    private Color initialColor;
+    //End of var declaration
+
+    private void Awake(){
+        
+        sr = GetComponent<SpriteRenderer>();
+
+    }
+
+    private void Start() {
+        
+        initialColor = sr.color;
+
+    }
+
+    // Update is called once per frame
+    private void Update(){
+        
+        if(Input.GetKeyDown(key))
+            StartCoroutine(ButtonPressed());
+
+        if(Input.GetKeyDown(key) && triggerIsActive)
+            Destroy(note);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D col){
+
+        triggerIsActive = true;
+        
+        if(col.gameObject.tag == "note")
+            note = col.gameObject;
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D col) {
+        
+        triggerIsActive = false;
+
+    }
+
+    IEnumerator ButtonPressed(){
+        
+        sr.color = new Color(40,150,200);
+
+        yield return new WaitForSeconds(0.1f);
+        sr.color = initialColor;
+
+    }
+
+}
---
+++ Assets/Scripts/Gameplay/Note.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float beatsPerMinute = 120;
+    private float beatsPerSecond, velocity, distance, yAxisValue;
+    
+
+    private float getVelocityForY(float beatsPerMinute, GameObject controller){
+
+        beatsPerSecond = beatsPerMinute / 60;
+        distance = transform.position.y - controller.transform.position.y;
+        velocity = distance * beatsPerSecond;
+
+        return velocity;
+
+    }
+
+    private void Awake() {
+
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Start is called before the first frame update
+    void Start(){
+        
+        GameObject controller = GameObject.FindGameObjectWithTag("controller");
+        
+        //Setting up the velocity between the note and the controller
+        //based on the BPM of the song 
+
+        yAxisValue = getVelocityForY(beatsPerMinute, controller) * -1;
+        rb.velocity = new Vector2(0, yAxisValue);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+}
